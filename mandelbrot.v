f24368c2cb9e6e87db8a84e7e32d4e873d4ffd61
Henrixounez <henrix.ounez@yahoo.fr>
2020-10-13 16:25:19 +0200

    concurrent mandelbrot computations

--- mandelbrot.v
+++ mandelbrot.v
@@ -2,17 +2,23 @@ import math
 import readline
 import term
 import os
+import runtime
+import sync
 
 const (
 	keys = [`w`, `a`, `s`, `d`, `q`, `e`, `i`, `k`]
 	// keys = [`z`, `q`, `s`, `d`, `a`, `e`, `i`, `k`]
 )
 
+struct Args {
+mut:
+	colored bool
+	verbose bool
+}
 struct Pixel {
 	r int
 	g int
 	b int
-	c byte
 }
 struct Mandel {
 	xmin f64
@@ -22,6 +28,10 @@ struct Mandel {
 	iterations int
 	width int
 	height int
+}
+struct ValPos {
+	c f64
+	pos int
 }
 
 fn mandel_iter(cx f64, cy f64, maxIter int) f64 {
@@ -42,42 +52,102 @@ fn mandel_iter(cx f64, cy f64, maxIter int) f64 {
 	return maxIter - i
 }
 
-fn (m Mandel)get_pixel(ix int, iy int) Pixel {
+fn (m Mandel)get_pixel(ix int, iy int) f64 {
 	x := m.xmin + (m.xmax - m.xmin) * ix / (m.width - 1)
 	y := m.ymin + (m.ymax - m.ymin) * iy / (m.height - 1)
 	i := mandel_iter(x, y, m.iterations)
 
 	if i > m.iterations {
-		return Pixel {0, 0, 0, `#`}
+		return -1
 	} else {
 		c := 3 * math.log(i) / math.log(f64(m.iterations) - 1.0)
-		if c < 1 {
-			return Pixel {int(255 * c), 0, 0, `+`}
-		} else if c < 2 {
-			return Pixel {255, int(255 * (c - 1)), 0, `.`}
-		} else {
-			return Pixel {255, 255, int(255 * (c - 2)), ` `}
-		}
+		return c
 	}
 }
 
-fn mandelbrot(xmin f64, xmax f64, ymin f64, ymax f64, iterations int, colored bool) {
-	width, height := term.get_terminal_size()
-	m := Mandel{ xmin xmax ymin ymax iterations width height }
+fn work_processor(mut work sync.Channel, mut results sync.Channel, mut wg sync.WaitGroup, m Mandel) {
+	for {
+		mut coord := u64(0)
+		if !work.pop(&coord) {
+			break
+		}
+		y := int(coord >> 32)
+		x := int(coord & 0xFFFFFFFF)
+		val := m.get_pixel(x, y)
+		valpos := ValPos{val, y * m.width + x}
+		results.push(&valpos)
+	}
+	wg.done()
+}
 
-	if !colored {
-		for iy in 0..height {
-			for ix in 0..width {
-				pix := m.get_pixel(ix, iy)
-				print(pix.c)	
+fn get_color(c f64) Pixel {
+	if c == -1 {
+		return Pixel {0, 0, 0}
+	} else if c < 1 {
+		return Pixel {int(255 * c), 0, 0}
+	} else if c < 2 {
+		return Pixel {255, int(255 * (c - 1)), 0}
+	} else {
+		return Pixel {255, 255, int(255 * (c - 2))}
+	}
+}
+
+fn mandelbrot(xmin f64, xmax f64, ymin f64, ymax f64, iterations int, args Args) {
+	width, height := term.get_terminal_size()
+	mut max_height := if args.colored { height * 2 } else { height }
+	m := Mandel{ xmin xmax ymin ymax iterations width max_height }
+
+	vjobs := runtime.nr_jobs()
+	mut work := sync.new_channel<u64>(max_height * width)
+	mut results := sync.new_channel<ValPos>(max_height * width)
+	mut wg := sync.new_waitgroup()
+
+	for y in 0..max_height {
+		for x in 0..width {
+			val := u64((u64(y) << 32) + u64(x))
+			unsafe {
+				work.push(&val)
+			}
+		}
+	}
+	work.close()
+	wg.add(vjobs)
+	for _ in 0..vjobs {
+		go work_processor(mut work, mut results, mut wg, m)
+	}
+	wg.wait()
+	mut arr_c := []f64{len: max_height * width,  init: -1.0}
+	for _ in 0..max_height {
+		for _ in 0..width {
+			mut valpos := ValPos{}
+			results.pop(&valpos)
+			arr_c[valpos.pos] = valpos.c
+		}
+	}
+	if args.verbose {
+		println('xmin: $xmin, xmax: $xmax, ymin: $ymin, ymax: $ymax, iterations: $iterations')
+	}
+	if !args.colored {
+		for y in 0..max_height {
+			for x in 0..width {
+				c := arr_c[y * width + x]
+				if c == -1 {
+					print('#')
+				} else if c < 1 {
+					print('+')
+				} else if c < 2 {
+					print('.')
+				} else {
+					print(' ')
+				}
 			}
 			print('\n')
 		}
 	} else {
-		for iy := 0; iy < height * 2; iy += 2 {
-			for ix in 0..width {
-				pix1 := m.get_pixel(ix, (iy / 2))
-				pix2 := m.get_pixel(ix, (iy / 2) + 1)
+		for y := 0; y < max_height; y += 2 {
+			for x in 0..width {
+				pix1 := get_color(arr_c[y * width + x])
+				pix2 := get_color(arr_c[(y + 1) * width + x])
 				print(term.bg_rgb(pix2.r, pix2.g, pix2.b, term.rgb(pix1.r, pix1.g, pix1.b, '▀')))
 			}
 			print('\n')
@@ -86,7 +156,14 @@ fn mandelbrot(xmin f64, xmax f64, ymin f64, ymax f64, iterations int, colored bo
 }
 
 fn main() {
-	colored := os.args.len == 2 && os.args[1] == '-color'
+	mut args := Args{}
+	for arg in os.args {
+		match arg {
+			'-color' { args.colored = true }
+			'-verbose' { args.verbose = true }
+			else {}
+		}
+	}
 	mut pointx := 0.0
 	mut pointy := 0.0
 	mut zoom := 1.0	
@@ -94,7 +171,7 @@ fn main() {
 	width, height := term.get_terminal_size()
 	ratio := f64(width) / f64(height)
 
-	println(ratio)
+
 	for {
 		mut r := readline.Readline{}
 		r.enable_raw_mode_nosig()
@@ -105,10 +182,9 @@ fn main() {
 			pointy - zoom,
 			pointy + zoom,
 			int(iter),
-			colored
+			args
 		)
 		input := r.read_char()
-		// input := os.input('> ')
 		match byte(input) {
 			keys[0] {
 				pointy -= zoom
@@ -124,10 +200,10 @@ fn main() {
 			}
 			keys[4] {
 				zoom *= 1.5
-				iter *= 0.75
+				iter *= 0.80
 			}
 			keys[5] {
-				zoom *= 0.5
+				zoom *= f64(2) / 3
 				iter *= 1.25
 			}
 			keys[6] {
